8b2dfe80600b61dbbef0eefbfa5d9d00f4710ebe
Abdullah Amin <abdullahamin231@gmail.com>
2025-10-30 13:25:41 +0500

    project structure: divide code into v1 and v2

--- src/V1/convolve.h
+++ src/V1/convolve.h
@@ -8,16 +8,6 @@
 #include "klt.h"
 #include "klt_util.h"
 
-
-#ifndef KLT_MAX_KERNEL_WIDTH
-#define KLT_MAX_KERNEL_WIDTH 71
-#endif
-
-typedef struct  {
-  int width;
-  float data[KLT_MAX_KERNEL_WIDTH];
-} KLT_ConvolutionKernel;
-
 void _KLTToFloatImage(
   KLT_PixelType *img,
   int ncols, int nrows,
@@ -29,43 +19,14 @@ void _KLTComputeGradients(
   _KLT_FloatImage gradx,
   _KLT_FloatImage grady);
 
-void _KLTComputeGradientsCPU(
-  _KLT_FloatImage img,
-  float sigma,
-  _KLT_FloatImage gradx,
-  _KLT_FloatImage grady);
-
 void _KLTGetKernelWidths(
   float sigma,
   int *gauss_width,
   int *gaussderiv_width);
 
-void _KLTGetKernels(
-  float sigma,
-  KLT_ConvolutionKernel *gauss,
-  KLT_ConvolutionKernel *gaussderiv);
-
 void _KLTComputeSmoothedImage(
   _KLT_FloatImage img,
   float sigma,
   _KLT_FloatImage smooth);
-
-void _KLTComputeSmoothedImageCPU(
-  _KLT_FloatImage img,
-  float sigma,
-  _KLT_FloatImage smooth);
-
-#ifdef KLT_USE_CUDA
-void _KLTComputeGradientsGPU(
-  _KLT_FloatImage img,
-  float sigma,
-  _KLT_FloatImage gradx,
-  _KLT_FloatImage grady);
-
-void _KLTComputeSmoothedImageGPU(
-  _KLT_FloatImage img,
-  float sigma,
-  _KLT_FloatImage smooth);
-#endif
 
 #endif
